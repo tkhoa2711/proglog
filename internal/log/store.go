d0fa5e06e8d7e5618de846fa1c2c8625274793b4
Khoa Le <ltkhoa2711@gmail.com>
2021-05-15 20:35:55 +1000

    feat: append and read records from the store

--- internal/log/store.go
+++ internal/log/store.go
@@ -2,7 +2,17 @@ package log
 
 import (
 	"bufio"
+	"encoding/binary"
 	"os"
+	"sync"
+)
+
+var (
+	encoding = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
 )
 
 // A wrapper around physical files to store records in
@@ -10,6 +20,7 @@ type store struct {
 	*os.File
 	buf  *bufio.Writer
 	size uint64
+	mu   sync.Mutex
 }
 
 func newStore(f *os.File) (*store, error) {
@@ -24,4 +35,81 @@ func newStore(f *os.File) (*store, error) {
 		size: size,
 		buf:  bufio.NewWriter(f),
 	}, nil
+}
+
+// Append persists the given bytes to the store. It returns the number of bytes
+// written, the position where the store holds the record in its file, and error
+// if any.
+func (s *store) Append(b []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	pos = s.size
+
+	// Write the length of the record so that when reading the record, we know
+	// how many bytes to read
+	if err := binary.Write(s.buf, encoding, uint64(len(b))); err != nil {
+		return 0, 0, err
+	}
+
+	// Write the record data
+	numBytesWritten, err := s.buf.Write(b)
+	if err != nil {
+		return 0, 0, nil
+	}
+
+	numBytesWritten += lenWidth
+	s.size += uint64(numBytesWritten)
+	return uint64(numBytesWritten), pos, nil
+}
+
+// Read returns the record stored at a given position and the error, if any
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// Make sure we flush all data in the buffer before reading the file
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	// Find out how many bytes we need in order to fetch the record
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	// Retrieve the record data as bytes
+	b := make([]byte, encoding.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// ReadAt reads len(b) bytes starting at the offset off from the store's file.
+// It returns the number of bytes read and the error, if any.
+func (s *store) ReadAt(b []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// Flush any buffered data before the read
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(b, off)
+}
+
+// Close closes the file and also persists any buffered data before doing so
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
 }
---
+++ internal/log/store_test.go
@@ -0,0 +1,129 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	recordData  = []byte("hellow world")
+	recordWidth = uint64(len(recordData)) + lenWidth
+)
+
+func TestAppend(t *testing.T) {
+	// TODO: can we extract store creation logic into its own function?
+	f, err := ioutil.TempFile("", "store_append_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+}
+
+func TestAppendThenRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	// write 4 records with the same data to the store
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(recordData)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, recordWidth*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		got, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, recordData, got)
+		pos += recordWidth
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		// Read the length of the record
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+
+		require.NoError(t, err)
+		require.Equal(t, int(lenWidth), n)
+
+		off += int64(n)
+
+		// Read the record itself
+		size := encoding.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+
+		require.NoError(t, err)
+		require.Equal(t, recordData, b)
+		require.Equal(t, int(size), n)
+
+		off += int64(n)
+	}
+}
+
+func TestClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	_, _, err = s.Append(recordData)
+	require.NoError(t, err)
+
+	f, beforeCloseSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterCloseSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterCloseSize > beforeCloseSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return f, fi.Size(), nil
+}
